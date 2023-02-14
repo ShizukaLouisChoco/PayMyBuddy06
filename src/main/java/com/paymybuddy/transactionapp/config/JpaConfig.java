2a2e797c63716392741858fe51ef8c15afabb13e
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-02-14 18:57:12 +0100

    Correction of config classes

--- src/main/java/com/paymybuddy/transactionapp/config/JpaConfig.java
+++ src/main/java/com/paymybuddy/transactionapp/config/JpaConfig.java
@@ -1,93 +1,21 @@
 package com.paymybuddy.transactionapp.config;
 
-import com.paymybuddy.transactionapp.entity.Balance;
-import com.paymybuddy.transactionapp.entity.Transaction;
-import com.paymybuddy.transactionapp.entity.UserAccount;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
-import java.util.Properties;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.paymybuddy.transactionapp.repository")
 public class JpaConfig {
 
-    @Autowired
-    private EntityManager entityManager;
-
-    public void saveUserAccount(UserAccount userAccount) {
-        entityManager.persist(userAccount);
-    }
-
-    public void saveBalance(Balance balance) {
-        entityManager.persist(balance);
-    }
-
-    public void saveTransaction(Transaction transaction) {
-        entityManager.persist(transaction);
-    }
-    @Qualifier("jpaDataSource")
     @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .build();
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource());
-        factoryBean.setPackagesToScan("com.paymybuddy.transactionapp");
-        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        factoryBean.setJpaProperties(jpaProperties());
-        return factoryBean;
-    }
-    /*
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter){
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setDataSource(dataSource);
-        factory.setJpaVendorAdapter(jpaVendorAdapter);
-        factory.setPackagesToScan("com.paymybuddy.transactionapp.entity");
+    public Jackson2RepositoryPopulatorFactoryBean getRepositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{
+                new ClassPathResource("user_account.json")
+        });
         return factory;
-    }*/
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.H2);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(false);
-        adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
-        return adapter;
     }
-
-    private Properties jpaProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
-        properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        return properties;
-    }
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new JpaTransactionManager(entityManagerFactory().getObject());
-    }
-
 
 }
--- src/main/java/com/paymybuddy/transactionapp/config/WebSecurityConfig.java
+++ src/main/java/com/paymybuddy/transactionapp/config/WebSecurityConfig.java
@@ -3,65 +3,90 @@ package com.paymybuddy.transactionapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.sql.DataSource;
+/*
+ * Filter chain configuration
+ * */
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Autowired
-    DataSource dataSource;
-    @Autowired
-    UserDetailsService userDetailsService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    private UserDetailsService userDetailsService;
 
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/","/home").permitAll()
-                        .anyRequest().authenticated())
-                        .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-        jdbcUserDetailsManager.setDataSource(dataSource);
-        return jdbcUserDetailsManager;
-    }
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/signin", "/h2-console/**").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/home", true)
+                .permitAll()
+//                .and()
+//                .rememberMe()
+//                .rememberMeParameter("rememberMe")
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .permitAll()
+                .and()
+                .csrf().ignoringRequestMatchers("/h2-console/**")
+                .and()
+                .headers().frameOptions().sameOrigin();
+
+
+//                .requestMatchers(/*"/","/signin","h2-console/**"*/"/**").permitAll()
+//                .requestMatchers("/admin").hasAnyAuthority("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login.html")
+//                .usernameParameter("email")
+//                .permitAll()
+//                .and()
+//                .userDetailsService(userDetailsService)
+//                .logout()
+//                .invalidateHttpSession(true)
+//                .clearAuthentication(true)
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+//                .and()
+//                .rememberMe()
+//                .key("uniqueAndSecret")
+//                .rememberMeParameter("rememberMe")
+//                .tokenValiditySeconds(24 * 60 * 60)
+//                .and()
+//                .formLogin()
+//                .defaultSuccessUrl("/home", true);
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/static/css/**", "/js/**");
     }
 
 }
