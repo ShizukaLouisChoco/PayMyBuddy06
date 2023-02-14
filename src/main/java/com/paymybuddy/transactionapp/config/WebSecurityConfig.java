e649c350e3e89b480ada710f68bfa176322da06b
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-02-14 19:51:58 +0100

    correction for config, rename

--- src/main/java/com/paymybuddy/transactionapp/config/WebSecurityConfig.java
+++ src/main/java/com/paymybuddy/transactionapp/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -17,10 +18,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
 
 
     @Bean
