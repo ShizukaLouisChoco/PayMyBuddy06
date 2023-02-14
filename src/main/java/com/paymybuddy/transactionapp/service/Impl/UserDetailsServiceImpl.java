6cb7c9052464b1c09c762b4f11103c66791ad321
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-02-14 19:20:09 +0100

    Correction of userDetailsService

--- src/main/java/com/paymybuddy/transactionapp/service/Impl/UserDetailsServiceImpl.java
+++ src/main/java/com/paymybuddy/transactionapp/service/Impl/UserDetailsServiceImpl.java
@@ -1,19 +1,17 @@
 package com.paymybuddy.transactionapp.service.Impl;
 
-import com.paymybuddy.transactionapp.entity.UserAccount;
 import com.paymybuddy.transactionapp.exception.UserAccountNotFoundException;
 import com.paymybuddy.transactionapp.repository.UserAccountRepository;
-import com.paymybuddy.transactionapp.service.UserDetailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import java.util.Optional;
-
+@Slf4j
 @Service
-public class UserDetailsServiceImpl implements UserDetailService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserAccountRepository userAccountRepository;
 
