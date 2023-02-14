0fc6ecd7330cc704b396cc39ff8a42cdfada24b0
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-02-14 18:50:41 +0100

    Delete unnecessary files

--- src/main/java/com/paymybuddy/transactionapp/dao/AbstractDataLoader.java
+++
@@ -1,7 +0,0 @@
-package com.paymybuddy.transactionapp.dao;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class AbstractDataLoader {
-    ObjectMapper mapper = new ObjectMapper();
-}
