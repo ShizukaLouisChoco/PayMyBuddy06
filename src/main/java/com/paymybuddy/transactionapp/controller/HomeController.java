7750bc6599513487b5677ffdc48fd5062a8ecacf
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-02-20 19:55:39 +0100

    correction for home page

--- src/main/java/com/paymybuddy/transactionapp/controller/HomeController.java
+++ src/main/java/com/paymybuddy/transactionapp/controller/HomeController.java
@@ -2,10 +2,14 @@ package com.paymybuddy.transactionapp.controller;
 
 import com.paymybuddy.transactionapp.entity.UserAccount;
 import com.paymybuddy.transactionapp.service.UserAccountService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -16,11 +20,29 @@ public class HomeController {
         this.userAccountService = userAccountService;
     }
 
+    /**
+     * logged in user can enter this page.
+     * url : "<a href="http://localhost:8080/login">...</a>"
+     * @return UserAccountServiceDto
+     */
 
     @GetMapping("/home")
-    public String home(Authentication authentication, Model model){
-        model.addAttribute("userAccount", authentication.getName());
+    public String home(Model model){
+        model.addAttribute("userAccount", userAccountService.getConnectedUser());
         return "home";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model){
+        model.addAttribute("userAccount",userAccountService.getConnectedUser());
+        model.addAttribute("email",new String());
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String addContact(@ModelAttribute("email")String email){
+        userAccountService.addFriend(email);
+        return"redirect:/contact";
     }
 }
 
