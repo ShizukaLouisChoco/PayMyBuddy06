bd29546b8c15ca5c3c946e4005503a9390ffa1ff
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-03-01 11:54:47 +0100

    correction for contact page

---
+++ src/main/java/com/paymybuddy/transactionapp/controller/ContactController.java
@@ -0,0 +1,40 @@
+package com.paymybuddy.transactionapp.controller;
+
+import com.paymybuddy.transactionapp.service.UserAccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ContactController {
+
+    UserAccountService userAccountService;
+
+    public ContactController(UserAccountService userAccountService){
+        this.userAccountService = userAccountService;
+    }
+
+    /**
+     * connected user can enter this page.
+     * url : "<a href="http://localhost:8080/contact">...</a>"
+     */
+    @GetMapping("/contact")
+    public String contact(Model model){
+        model.addAttribute("userAccount",userAccountService.getConnectedUser());
+        model.addAttribute("email",new String());
+        model.addAttribute("connections",userAccountService.getConnectedUser().getConnections());
+        return "contact";
+    }
+    /**
+     * connected user can add friend in this page.
+     * url : "<a href="http://localhost:8080/contact">...</a>"
+     */
+
+    @PostMapping("/contact")
+    public String addContact(@ModelAttribute("email")String email){
+        userAccountService.addFriend(email);
+        return"redirect:/contact";
+    }
+}
