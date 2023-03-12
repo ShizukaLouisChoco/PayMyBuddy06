d5f95914bc6fd821cdc1109328c4bfad56737471
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-03-12 18:13:27 +0100

    correction for TransactionServiceTest - use @InjectMocks - add tests

--- src/test/java/com/paymybuddy/transactionapp/service/TransactionServiceTest.java
+++ src/test/java/com/paymybuddy/transactionapp/service/TransactionServiceTest.java
@@ -4,15 +4,18 @@ import com.paymybuddy.transactionapp.dto.TransactionDto;
 import com.paymybuddy.transactionapp.entity.Transaction;
 import com.paymybuddy.transactionapp.entity.UserAccount;
 import com.paymybuddy.transactionapp.repository.TransactionRepository;
+import com.paymybuddy.transactionapp.service.Impl.TransactionServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,16 +24,14 @@ import static org.mockito.Mockito.*;
 @ExtendWith(SpringExtension.class)
 public class TransactionServiceTest {
 
-    @Mock
-    private TransactionRepository transactionRepository;
-
-    @MockBean
-    private TransactionService transactionService;
-    @Mock
-    private UserAccountService userAccountService;
-
+    @InjectMocks
+    TransactionServiceImpl transactionService;
     @Mock
     ConnectedUserDetailsService connectedUserDetailsService;
+    @Mock
+    UserAccountService userAccountService;
+    @Mock
+    TransactionRepository transactionRepository;
 
 
     @Test
@@ -85,5 +86,30 @@ public class TransactionServiceTest {
         verify(transactionRepository,times(1)).findAllByCreditorId(debtorAccount.getId());
 
     }
+    @Test
+    @DisplayName("TransactionService calls transactionRepository findByDebtor method ")
+    public void paginatedTest(){
+        // GIVEN
 
+        UserAccount creditorUserAccount = new UserAccount(2L, "creditor@email.com","creditor","creditor",null,null);
+        UserAccount connectedUserAccount = new UserAccount(1L, "debtor@email.com","debtor","debtor",null, List.of(creditorUserAccount));
+        Transaction transaction1 = new Transaction(new TransactionDto(creditorUserAccount.getId(),BigDecimal.valueOf(15),"transaction1"),connectedUserAccount.getId());
+        Transaction transaction2 = new Transaction(new TransactionDto(creditorUserAccount.getId(),BigDecimal.valueOf(15),"transaction2"),connectedUserAccount.getId());
+        List<Transaction> transactionList = new ArrayList<Transaction>();
+        transactionList.add(transaction1);
+        transactionList.add(transaction2);
+        int pageSize = 2;
+        int pageNumber = 0;
+        Sort sort = Sort.by(Sort.Direction.DESC, "transactionId"); // ソート条件
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        Page<Transaction> expectedResult = new PageImpl<Transaction>(transactionList, pageable, transactionList.size());
+
+        // WHEN
+        when(connectedUserDetailsService.getConnectedUser()).thenReturn(connectedUserAccount);
+        when(transactionRepository.findAllByDebtorId(connectedUserAccount.getId(),pageable)).thenReturn(expectedResult);
+        var result = transactionService.findPaginated(pageable);
+        // THEN
+        verify(transactionRepository,times(1)).findAllByDebtorId(connectedUserAccount.getId(),pageable);
+        assertThat(result).isEqualTo(expectedResult);
+    }
 }
