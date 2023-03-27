e56391fd553d53a0bff131b4902eac321c03d7de
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-03-27 22:16:03 +0200

    correction for LoginControllerIT

--- src/test/java/com/paymybuddy/transactionapp/controller/LoginControllerIT.java
+++ src/test/java/com/paymybuddy/transactionapp/controller/LoginControllerIT.java
@@ -1,23 +1,24 @@
 package com.paymybuddy.transactionapp.controller;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(LoginController.class)
-public class LoginControllerIT { @Autowired
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerIT {
+
+    @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("loginPage displays login.html")
     @Test
+    @DisplayName("loginPage displays login.html")
     public void testLoginPage() throws Exception {
         mockMvc.perform(get("/login"))
                 .andExpect(status().isOk());
