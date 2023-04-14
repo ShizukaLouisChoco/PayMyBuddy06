9023aa86e49f7a977216e3d50ca53a5a1ff5d0bd
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-04-14 11:31:05 +0200

    update readme

--- README.md
+++ README.md
@@ -92,7 +92,34 @@ Once the application is running, open your web browser and navigate to http://lo
 ![UML.png](docs/UML.png)
 
 # Database Model
-![dataModel.png](docs/dataModel.png)
+![dataModelIE.png](docs/dataModelIE.png)
 
 
+# Script mySQL
+```bash
+drop table if exists transaction;
+drop table if exists user_account;
+drop table if exists user_account_friends;
+
+create table transaction (
+transaction_id bigint not null auto_increment,
+amount decimal(38,2) not null,
+creditAmount decimal(38,2),
+description varchar(255) not null,
+creditor_id bigint not null,
+debtor_id bigint not null,
+primary key (transaction_id));
+
+create table user_account (
+id bigint not null auto_increment,
+balance decimal(38,2),
+email varchar(30) not null,
+password varchar(255) not null,
+username varchar(255) not null,
+primary key (id));
+
+create table user_account_friends (
+UserAccount_id bigint not null,
+friends_id bigint not null);
+```
 
--- docs/schema.sql
+++ docs/schema.sql
@@ -2,31 +2,23 @@ drop table if exists transaction;
 drop table if exists user_account;
 drop table if exists user_account_friends;
 
-CREATE TABLE user_account (
-id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(30) NOT NULL,
-username VARCHAR(255) NOT NULL,
-password VARCHAR(255) NOT NULL,
-balance DECIMAL(19,2) DEFAULT '0.00');
+create table transaction (
+                             transaction_id bigint not null auto_increment,
+                             amount decimal(38,2) not null,
+                             creditAmount decimal(38,2),
+                             description varchar(255) not null,
+                             creditor_id bigint not null,
+                             debtor_id bigint not null,
+                             primary key (transaction_id));
 
-CREATE TABLE transaction (
-transaction_id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-amount DECIMAL(19,2),
-credit_amount DECIMAL(19,2),
-description VARCHAR(255),
-creditor_id BIGINT(20),
-debtor_id BIGINT(20));
+create table user_account (
+                              id bigint not null auto_increment,
+                              balance decimal(38,2),
+                              email varchar(30) not null,
+                              password varchar(255) not null,
+                              username varchar(255) not null,
+                              primary key (id));
 
-CREATE TABLE user_account_friends (
-user_account_id BIGINT(20) NOT NULL,
-friends_id BIGINT(20) NOT NULL,
-PRIMARY KEY (user_account_id, friends_id),
-CONSTRAINT fk_user_account
-FOREIGN KEY (user_account_id)
-REFERENCES user_account (id)
-ON DELETE CASCADE,
-CONSTRAINT fk_friends
-FOREIGN KEY (friends_id)
-REFERENCES user_account (id)
-ON DELETE CASCADE
-);
+create table user_account_friends (
+                                      UserAccount_id bigint not null,
+                                      friends_id bigint not null);
